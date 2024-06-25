9ecd19de24701115619804a44457f955406c510f
Laura Sinclair <l.sinclair@tuta.io>
2024-06-25 12:26:47 +0200

    Adding info for redirect

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-base: process.env.NODE_ENV === 'production' ? '/projects/memoreel/' : '/',
-  plugins: [react()],
+	base: process.env.NODE_ENV === 'production' ? '/projects/memoreel/' : '/',
+	plugins: [react()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
@@ -15,10 +15,10 @@ base: process.env.NODE_ENV === 'production' ? '/projects/memoreel/' : '/',
 			'@images': `${path.resolve(__dirname, 'src/assets/images/')}`,
 			'@services': `${path.resolve(__dirname, 'src/services/')}`,
 			'@pages': path.resolve(__dirname, 'src/pages'),
-			'@utils': path.resolve(__dirname, 'src/utils')
+			'@utils': path.resolve(__dirname, 'src/utils'),
 		},
 	},
-  build: {
-    outDir: 'dist'
-  }
-})
+	build: {
+		outDir: '/projects/memoreel/',
+	},
+});
