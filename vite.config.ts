a6d9aed7c2437f26b14b5000f7a82becf45af6d9
Laura Sinclair <l.sinclair@tuta.io>
2025-08-25 20:38:40 +0200

    Update vite aliases (now TS aliases)

--- vite.config.ts
+++ vite.config.ts
@@ -13,27 +13,3 @@ export default defineConfig({
 		outDir: "/projects/memoreel/",
 	},
 });
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-// 	base: process.env.NODE_ENV === "production" ? "/projects/memoreel/" : "/",
-// 	plugins: [react(), tsconfigPaths()],
-// 	resolve: {
-// 		alias: {
-// 			src: path.resolve(__dirname, "src"),
-// 			assets: `${path.resolve(__dirname, "src/assets/")}`,
-// 			components: `${path.resolve(__dirname, "src/components/")}`,
-// 			context: `${path.resolve(__dirname, "src/context/")}`,
-// 			images: `${path.resolve(__dirname, "src/assets/images/")}`,
-// 			pages: path.resolve(__dirname, "src/pages"),
-// 			paths: path.resolve(__dirname, "src/paths"),
-// 			router: path.resolve(__dirname, "src/router"),
-// 			services: `${path.resolve(__dirname, "src/services/")}`,
-// 			types: path.resolve(__dirname, "src/types"),
-// 			utils: path.resolve(__dirname, "src/utils"),
-// 		},
-// 	},
-// 	build: {
-// 		outDir: "/projects/memoreel/",
-// 	},
-// });
