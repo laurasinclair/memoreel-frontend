081d5f9ed646a42e41499746d76a6f5e1a9f0f08
Laura Sinclair <l.sinclair@tuta.io>
2025-08-26 01:03:20 +0200

    Add TanStack

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import Router from "router";
 import './styles/index.sass';
@@ -6,11 +7,14 @@ import React from 'react';
 import { AuthProviderWrapper } from './context';
 
 window.name = 'MemoReel';
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<AuthProviderWrapper>
-			<RouterProvider router={Router} />
-		</AuthProviderWrapper>
+		<QueryClientProvider client={queryClient}>
+			<AuthProviderWrapper>
+				<RouterProvider router={Router} />
+			</AuthProviderWrapper>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
