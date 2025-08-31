373f88856a790226d4b48c87bfe1eec6c2109c67
Laura Sinclair <l.sinclair@tuta.io>
2025-08-31 22:43:29 +0200

    Little logger update to prevent turning what is passed into strings

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,3 @@
-import { LoggerMessageType } from "src/types";
-
 const logLevels = {
     log: "🟢",
     warn: "🟠",
@@ -13,7 +11,7 @@ const getCallerInfo = () => {
         const callerLine = stackLines[3]; // the caller is typically on the 3rd line of the stack trace
         if (callerLine) {
             const match = callerLine.match(/at\s+(.*)\s+\(/);
-            return match ? match[1] : "unknown";
+            return match ? match[1] : "";
         }
     }
     return "unknown";
@@ -22,14 +20,11 @@ const getCallerInfo = () => {
 const logger = Object.fromEntries(
     Object.entries(logLevels).map(([level, icon]) => [
         level,
-        (message: LoggerMessageType) => {
+        (message) => {
             if (process.env.NODE_ENV !== "production") {
                 const callerInfo = getCallerInfo();
-                console[level](
-							`${icon} %c${callerInfo} %c${message}`,
-							"color: #ffffff9f; background-color: #ffffff32; padding: 2px 1px 1px 8px !important; margin-right: 6px; border-radius: 3px;",
-							"color: initial"
-						);
+                const callerInfoText = callerInfo ? `[${callerInfo}]` : "";
+                console[level](`${icon} ${callerInfoText}`, message);
             }
         },
     ])
